dceac8446f6e7c1a9be7ad6500b8902003774c5a
Nuttapat04 <pjelato@gmail.com>
2024-11-06 16:17:32 +0700

    connect.php

--- ChichHub/Component/Admin/connect.php
+++ ChichHub/Component/Admin/connect.php
@@ -1,8 +1,5 @@
 <?php
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=sec2_18;charset=utf8", "Tstd18", "64gJY16w");
+    $pdo = new PDO("mysql:host=localhost;dbname=ChicHub;charset=utf8", "root", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-	echo "เกิดข้อผิดพลาด : ".$e->getMessage();
-}
 ?>
+
